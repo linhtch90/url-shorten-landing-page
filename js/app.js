07ca82c32621dd6f908cf7ce65e61b66c723e18a
Linh Truong Cong Hong <linhtch90@gmail.com>
2020-07-28 16:32:48 +0700

    Finish the script to shorten URL

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(function () {
+    var longUrl = '';
+
+    // Input Url validation
+    function validateURL(textval) {
+        var urlregex = new RegExp(
+            "^(http:\/\/www.|https:\/\/www.|ftp:\/\/www.|www.){1}([0-9A-Za-z]+\.)");
+        return urlregex.test(textval);
+    };
+
+    // Shorten link with Bitly
+    function shortenLink(longUrl) {
+        $.bitlr({
+            apiKey: '23c47f073826eb0cdc1c2abedbb006976e0e0549',
+            link: longUrl,
+            anchor: false,
+            success: function (shortUrl) {
+                $('#shortUrl span').text(shortUrl);
+            }
+        });
+    };
+
+    $('#shortUrlButton').click(function () {
+        var longUrl = $('#linkInput').val();
+        if (validateURL(longUrl)) {
+            shortenLink(longUrl);
+        } else {
+            alert("Please enter a valid URL!");
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
